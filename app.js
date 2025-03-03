df230d983f11e4be03b449a3b7562d8ed799c9af
Spencer Davis <spencerdavis226@gmail.com>
2025-03-03 14:31:50 -0500

    Added ejs functionality with basic CSS

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const express = require('express');
+const { lowerCase } = require('lodash');
 
 // Express App
 const app = express();
+
+// Register View Engine
+app.set('view engine', 'ejs');
 
 // Listen for requests on port 3000
 app.listen(3000); // Go to localhost:3000 on browser
 
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', { root: __dirname });
+  const blogs = [
+    { title: 'Yoshi finds eggs', snippet: 'laisdbnfliusdfvbwesfbviasdb' },
+    { title: 'Mario finds stars', snippet: 'dfiaklweucnqeebuoywerfnvweruivb' },
+    { title: 'How to defeat bowser', snippet: 'iasklcoimeruivbneiwuvbyuwoeb' },
+  ];
+  res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about', { title: 'About' });
 });
 
-// Redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new Blog' });
 });
 
 // 404 Page
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404', { title: '404' });
 });
