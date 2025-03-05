2302683dc170501bfa391950a8e7d82d637f9930
Spencer Davis <spencerdavis226@gmail.com>
2025-03-05 15:27:22 -0500

    Added deletion functionality

--- app.js
+++ app.js
@@ -52,6 +52,24 @@ app.post('/blogs', (req, res) => {
     .catch((err) => console.log(err));
 });
 
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch((err) => console.log(err));
+});
+
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new Blog' });
 });
