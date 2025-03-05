b76d9b591376dfdb25f0e07572c06a5cffe74243
Spencer Davis <spencerdavis226@gmail.com>
2025-03-05 14:17:14 -0500

    Added save, find, and find by ID functionality for blogs on MongoDB

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // Express App
 const app = express();
 
 // Connect to mongoDB
-const { dbUser, dbPassword } = require('./secrets');
-const dbURI = `mongodb+srv://${dbUser}:${dbPassword}@nodetuts.4lhc2.mongodb.net/`;
+const { username, password } = require('./secrets');
+const dbURI = `mongodb+srv://${username}:${password}@nodetuts.4lhc2.mongodb.net/node-tuts`;
 mongoose
   .connect(dbURI)
   .then((result) => app.listen(3000))
@@ -19,6 +20,38 @@ app.set('view engine', 'ejs');
 // MIDDLEWARE & STATIC FILES (CSS/images)
 app.use(express.static('public')); // Refers to folder name: public
 app.use(morgan('dev'));
+
+// Mongoose and mongo sandbox routes
+// SAVE
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog 2',
+    snippet: 'about my new blog',
+    body: 'more about my new blog',
+  });
+
+  blog
+    .save()
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err));
+});
+
+// FIND
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
+// FIND BY ID
+app.get('/single-blog', (req, res) => {
+  Blog.findById('67c8a143f74c3e402c40f86c') // ID grabbed from mongoDB of a blog
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
 
 app.get('/', (req, res) => {
   const blogs = [
--- models/blog.js
+++ models/blog.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const blogSchema = new Schema({
-  title:
-});
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    snippet: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Blog = mongoose.model('Blog', blogSchema); // .model('Blog') must match unpluralized name of collection on mongoDB cluster (eg. blogs)
+
+module.exports = Blog;
